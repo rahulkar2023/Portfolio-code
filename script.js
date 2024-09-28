07f372edc55c6d38f2a460e1aca2e0ffd1564933
Pritidev-Kar <125079002+rahulkar2023@users.noreply.github.com>
2024-09-28 21:41:40 +0530

    Add files via upload
    HII TO ALL THIS MY PERSONAL PORTFOLIO

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scroll for navbar links
+$(document).ready(function() {
+    $('.nav-link').on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        const hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800);
+      }
+    });
+  });
